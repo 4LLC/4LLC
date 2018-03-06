949310d684e35cc64b19a8d387b4d012008edda3
anujap <anujapatki87@gmail.com>
2018-03-06 15:26:10 -0600

    Settings

--- app/src/main/java/com/fourllc/donate/DonateBloodLocationActivity.java
+++ app/src/main/java/com/fourllc/donate/DonateBloodLocationActivity.java
@@ -1,5 +1,6 @@
 package com.fourllc.donate;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.PagerAdapter;
@@ -8,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.fourllc.donate.Adapters.DonateBloodLocationAdapter;
 
@@ -75,5 +77,19 @@ public class DonateBloodLocationActivity extends AppCompatActivity implements Ta
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main_menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch(id) {
+            case R.id.action_settings:
+                Intent intent  = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }

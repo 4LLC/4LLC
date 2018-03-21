3051ec868efcf863f7e5ba23f6a31137e398fb23
aaronbrecher <aribrecherdev@gmail.com>
2018-03-20 22:04:08 -0400

    Fixed the scrolling and tabs with Anuja's code began layout for donates

--- app/src/main/java/com/fourllc/donate/MainActivity.java
+++ app/src/main/java/com/fourllc/donate/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.fourllc.donate.Adapters.MainActivityPagerAdapter;
 
@@ -12,10 +13,12 @@ public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSe
     private TabLayout tabLayout;
     private ViewPager viewPager;
     private PagerAdapter pagerAdapter;
+    private Toolbar mToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
 
         tabLayout = (TabLayout) findViewById(R.id.main_activity_tab_layout);
         viewPager = (ViewPager) findViewById(R.id.main_activity_view_pager);
@@ -25,6 +28,13 @@ public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSe
         tabLayout.addTab(tabLayout.newTab().setText("Donate Money"));
         tabLayout.addTab(tabLayout.newTab().setText("Donate Blood"));
         tabLayout.setTabGravity(TabLayout.GRAVITY_CENTER);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setSupportActionBar(mToolbar);
+        if(getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
 
         //create a new adapter for the view pager to show the fragment
         pagerAdapter = new MainActivityPagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
--- app/src/main/java/com/fourllc/donate/fragments/mainActivityFragments/MainActivityDetailsFragment.java
+++ app/src/main/java/com/fourllc/donate/fragments/mainActivityFragments/MainActivityDetailsFragment.java
@@ -18,6 +18,6 @@ public class MainActivityDetailsFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.main_activity_details_fragment, container, false);
+        return inflater.inflate(R.layout.main_activity_details_fragment2, container, false);
     }
 }

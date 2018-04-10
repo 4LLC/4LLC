f9747d2225187a4d45dc48dd4cf235124f147bf4
aaronbrecher <aribrecherdev@gmail.com>
2018-04-10 00:10:17 -0400

    Added JustGiving Class with API strings, Set up button to launch Simple Donation Integration

--- app/src/main/java/com/fourllc/donate/MainActivity.java
+++ app/src/main/java/com/fourllc/donate/MainActivity.java
@@ -1,8 +1,14 @@
 package com.fourllc.donate;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+import com.fourllc.donate.NetworkingUtils.JustGiving;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main);
 
 
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -20,6 +26,23 @@ public class MainActivity extends AppCompatActivity {
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
+        Button donateBloodButton = (Button) findViewById(R.id.donateBloodButton);
+        Button donateMoneyButton = (Button) findViewById(R.id.donateMoneyButton);
 
+        donateMoneyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse(JustGiving.DONATION_URL);
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+        donateBloodButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //send user to the blood pledge page
+            }
+        });
     }
 }
--- app/src/main/java/com/fourllc/donate/paymentReceivedActivity.java
+++ app/src/main/java/com/fourllc/donate/paymentReceivedActivity.java
@@ -1,7 +1,12 @@
 package com.fourllc.donate;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.fourllc.donate.NetworkingUtils.JustGiving;
 
 public class paymentReceivedActivity extends AppCompatActivity {
 
@@ -9,5 +14,9 @@ public class paymentReceivedActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_received);
+
+        Intent intent = getIntent();
+        Uri data = intent.getData();
+        String donationId = data.getQueryParameter(JustGiving.DONATION_ID_PARAMETER);
     }
 }
